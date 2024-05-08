f7125f68d7459bbf18ecbbfffb6b29328f37e1fc
ralph8892 <brufalralph@gmail.com>
2024-05-08 22:27:47 +0800

    Minor changes for navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,8 +2,8 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <div class="container py-2">
             <a href="#" class="navbar-brand">
-                <span>Toedoe</span>
-                <strong>List</strong>
+                <span>Tasker</span>
+                <strong>VUE</strong>
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                 aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
