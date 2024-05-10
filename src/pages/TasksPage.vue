9c01bec2b6a051a0d670f1a9ba6d231e6ac82b78
ralph8892 <brufalralph@gmail.com>
2024-05-10 22:32:05 +0800

    Added new component New Task for inserting data

--- src/pages/TasksPage.vue
+++ src/pages/TasksPage.vue
@@ -4,14 +4,8 @@
             <div class="row">
                 <div class="col-md-8 offset-md-2">
                     <!-- Add new Task -->
-
-                    <div class="relative">
-
-                    <input type="text" name="add-task" class="form-control form-control-lg padding-right-lg"
-
-                        placeholder="+ Add new task. Press enter to save." />
-
-                    </div>
+                    <NewTask @added="handleAddedTask" />
+                    
 
                     <!-- List of incomplete tasks -->
 
@@ -34,8 +28,9 @@
 
 <script setup>
     import { computed, onMounted, ref } from "vue";
-    import { allTasks } from "../http/task-api";
+    import { allTasks, createTask } from "../http/task-api";
     import Tasks from "../components/tasks/Tasks.vue";
+    import NewTask from "../components/tasks/NewTask.vue";
 
     const tasks = ref([])
 
@@ -50,4 +45,9 @@
     const showToggleCompletedBtn = computed(() => uncompletedTasks.value.length > 0 && completedTasks.value.length > 0)
     const completedTasksIsVisible = computed(() => uncompletedTasks.value.length === 0 || completedTasks.value.length > 0)
     const showCompletedTasks = ref(false)
+
+    const handleAddedTask = async (newTask) => {
+        const { data: createdTask } = await createTask(newTask)
+        tasks.value.unshift(createdTask.data)
+    }
 </script>
